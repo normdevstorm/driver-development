55ab266dc3f67683552678e170f41bd0bee92583
normservercentos <normservercentos@localhost.localdomain>
2025-07-09 23:47:56 +0700

    feat: add textbox to the client ui

--- userspace/gtk_ui/chat_client_gtk.c
+++ userspace/gtk_ui/chat_client_gtk.c
@@ -21,6 +21,7 @@ typedef struct {
     GtkWidget *username_entry;
     GtkWidget *password_entry;
     GtkWidget *server_ip_entry;
+    GtkWidget *port_entry;
     GtkWidget *connect_button;
     GtkWidget *signup_button;
     GtkWidget *disconnect_button;
@@ -166,6 +167,10 @@ void on_connect_clicked(GtkButton *button, gpointer user_data) {
     const char *username = gtk_entry_get_text(GTK_ENTRY(app->username_entry));
     const char *password = gtk_entry_get_text(GTK_ENTRY(app->password_entry));
     const char *server_ip = gtk_entry_get_text(GTK_ENTRY(app->server_ip_entry));
+    const char *port_text = gtk_entry_get_text(GTK_ENTRY(app->port_entry));
+    
+    // Clear any previous status message
+    gtk_label_set_text(GTK_LABEL(app->status_label), "");
     
     if (strlen(username) == 0 || strlen(password) == 0) {
         GtkWidget *dialog = gtk_message_dialog_new(GTK_WINDOW(app->window),
@@ -183,6 +188,16 @@ void on_connect_clicked(GtkButton *button, gpointer user_data) {
         server_ip = "127.0.0.1";
     }
     
+    // Parse port
+    int port = SERVER_PORT; // Default port
+    if (strlen(port_text) > 0) {
+        port = atoi(port_text);
+        if (port <= 0 || port > 65535) {
+            gtk_label_set_text(GTK_LABEL(app->status_label), "Invalid port number (1-65535)");
+            return;
+        }
+    }
+    
     gtk_label_set_text(GTK_LABEL(app->status_label), "Connecting...");
     
     // Create socket
@@ -196,7 +211,7 @@ void on_connect_clicked(GtkButton *button, gpointer user_data) {
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_port = htons(port);
     
     // Use inet_pton for better IP address parsing
     if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
@@ -209,7 +224,7 @@ void on_connect_clicked(GtkButton *button, gpointer user_data) {
     
     if (connect(app->socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         char error_msg[256];
-        snprintf(error_msg, sizeof(error_msg), "Connection failed to %s:%d", server_ip, SERVER_PORT);
+        snprintf(error_msg, sizeof(error_msg), "Connection failed to %s:%d", server_ip, port);
         gtk_label_set_text(GTK_LABEL(app->status_label), error_msg);
         close(app->socket_fd);
         return;
@@ -255,6 +270,7 @@ void on_connect_clicked(GtkButton *button, gpointer user_data) {
             gtk_widget_set_sensitive(app->username_entry, FALSE);
             gtk_widget_set_sensitive(app->password_entry, FALSE);
             gtk_widget_set_sensitive(app->server_ip_entry, FALSE);
+            gtk_widget_set_sensitive(app->port_entry, FALSE);
             
             gtk_label_set_text(GTK_LABEL(app->status_label), "Connected");
             
@@ -292,6 +308,7 @@ void on_disconnect_clicked(GtkButton *button, gpointer user_data) {
         gtk_widget_set_sensitive(app->username_entry, TRUE);
         gtk_widget_set_sensitive(app->password_entry, TRUE);
         gtk_widget_set_sensitive(app->server_ip_entry, TRUE);
+        gtk_widget_set_sensitive(app->port_entry, TRUE);
         
         gtk_label_set_text(GTK_LABEL(app->status_label), "Disconnected");
         update_chat_display(g_strdup("Disconnected from server"));
@@ -306,11 +323,32 @@ void on_signup_clicked(GtkButton *button, gpointer user_data) {
     const char *username = gtk_entry_get_text(GTK_ENTRY(app->username_entry));
     const char *password = gtk_entry_get_text(GTK_ENTRY(app->password_entry));
     const char *server_ip = gtk_entry_get_text(GTK_ENTRY(app->server_ip_entry));
+    const char *port_text = gtk_entry_get_text(GTK_ENTRY(app->port_entry));
+    
+    // Clear any previous status message
+    gtk_label_set_text(GTK_LABEL(app->status_label), "");
     
     if (strlen(username) == 0 || strlen(password) == 0) {
         gtk_label_set_text(GTK_LABEL(app->status_label), "Please enter username and password");
         return;
     }
+    
+    if (strlen(server_ip) == 0) {
+        gtk_entry_set_text(GTK_ENTRY(app->server_ip_entry), "127.0.0.1");
+        server_ip = "127.0.0.1";
+    }
+    
+    // Parse port
+    int port = SERVER_PORT; // Default port
+    if (strlen(port_text) > 0) {
+        port = atoi(port_text);
+        if (port <= 0 || port > 65535) {
+            gtk_label_set_text(GTK_LABEL(app->status_label), "Invalid port number (1-65535)");
+            return;
+        }
+    }
+    
+    gtk_label_set_text(GTK_LABEL(app->status_label), "Connecting for signup...");
     
     // Create socket
     app->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -323,7 +361,7 @@ void on_signup_clicked(GtkButton *button, gpointer user_data) {
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_port = htons(port);
     
     // Use inet_pton for better IP address parsing
     if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
@@ -336,7 +374,7 @@ void on_signup_clicked(GtkButton *button, gpointer user_data) {
     
     if (connect(app->socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         char error_msg[256];
-        snprintf(error_msg, sizeof(error_msg), "Signup connection failed to %s:%d", server_ip, SERVER_PORT);
+        snprintf(error_msg, sizeof(error_msg), "Signup connection failed to %s:%d", server_ip, port);
         gtk_label_set_text(GTK_LABEL(app->status_label), error_msg);
         close(app->socket_fd);
         return;
@@ -464,7 +502,7 @@ GtkWidget* create_chat_window() {
     
     // Connection frame
     GtkWidget *connection_frame = gtk_frame_new("Connection");
-    GtkWidget *connection_table = gtk_table_new(4, 3, FALSE);
+    GtkWidget *connection_table = gtk_table_new(5, 3, FALSE);
     gtk_container_add(GTK_CONTAINER(connection_frame), connection_table);
     gtk_container_set_border_width(GTK_CONTAINER(connection_table), 5);
     
@@ -475,18 +513,25 @@ GtkWidget* create_chat_window() {
     gtk_table_attach(GTK_TABLE(connection_table), ip_label, 0, 1, 0, 1, GTK_FILL, GTK_FILL, 5, 2);
     gtk_table_attach(GTK_TABLE(connection_table), client_app->server_ip_entry, 1, 2, 0, 1, GTK_EXPAND | GTK_FILL, GTK_FILL, 5, 2);
     
+    // Port
+    GtkWidget *port_label = gtk_label_new("Port:");
+    client_app->port_entry = gtk_entry_new();
+    gtk_entry_set_text(GTK_ENTRY(client_app->port_entry), "8888");
+    gtk_table_attach(GTK_TABLE(connection_table), port_label, 0, 1, 1, 2, GTK_FILL, GTK_FILL, 5, 2);
+    gtk_table_attach(GTK_TABLE(connection_table), client_app->port_entry, 1, 2, 1, 2, GTK_EXPAND | GTK_FILL, GTK_FILL, 5, 2);
+    
     // Username
     GtkWidget *username_label = gtk_label_new("Username:");
     client_app->username_entry = gtk_entry_new();
-    gtk_table_attach(GTK_TABLE(connection_table), username_label, 0, 1, 1, 2, GTK_FILL, GTK_FILL, 5, 2);
-    gtk_table_attach(GTK_TABLE(connection_table), client_app->username_entry, 1, 2, 1, 2, GTK_EXPAND | GTK_FILL, GTK_FILL, 5, 2);
+    gtk_table_attach(GTK_TABLE(connection_table), username_label, 0, 1, 2, 3, GTK_FILL, GTK_FILL, 5, 2);
+    gtk_table_attach(GTK_TABLE(connection_table), client_app->username_entry, 1, 2, 2, 3, GTK_EXPAND | GTK_FILL, GTK_FILL, 5, 2);
     
     // Password
     GtkWidget *password_label = gtk_label_new("Password:");
     client_app->password_entry = gtk_entry_new();
     gtk_entry_set_visibility(GTK_ENTRY(client_app->password_entry), FALSE);
-    gtk_table_attach(GTK_TABLE(connection_table), password_label, 0, 1, 2, 3, GTK_FILL, GTK_FILL, 5, 2);
-    gtk_table_attach(GTK_TABLE(connection_table), client_app->password_entry, 1, 2, 2, 3, GTK_EXPAND | GTK_FILL, GTK_FILL, 5, 2);
+    gtk_table_attach(GTK_TABLE(connection_table), password_label, 0, 1, 3, 4, GTK_FILL, GTK_FILL, 5, 2);
+    gtk_table_attach(GTK_TABLE(connection_table), client_app->password_entry, 1, 2, 3, 4, GTK_EXPAND | GTK_FILL, GTK_FILL, 5, 2);
     
     // Connection buttons
     GtkWidget *button_hbox = gtk_hbox_new(FALSE, 5);
@@ -498,11 +543,11 @@ GtkWidget* create_chat_window() {
     gtk_box_pack_start(GTK_BOX(button_hbox), client_app->connect_button, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(button_hbox), client_app->signup_button, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(button_hbox), client_app->disconnect_button, TRUE, TRUE, 0);
-    gtk_table_attach(GTK_TABLE(connection_table), button_hbox, 0, 2, 3, 4, GTK_EXPAND | GTK_FILL, GTK_FILL, 5, 2);
+    gtk_table_attach(GTK_TABLE(connection_table), button_hbox, 0, 2, 4, 5, GTK_EXPAND | GTK_FILL, GTK_FILL, 5, 2);
     
     // Status label
     client_app->status_label = gtk_label_new("Not connected");
-    gtk_table_attach(GTK_TABLE(connection_table), client_app->status_label, 2, 3, 0, 4, GTK_FILL, GTK_FILL, 5, 2);
+    gtk_table_attach(GTK_TABLE(connection_table), client_app->status_label, 2, 3, 0, 5, GTK_FILL, GTK_FILL, 5, 2);
     
     gtk_box_pack_start(GTK_BOX(main_vbox), connection_frame, FALSE, FALSE, 0);
     
